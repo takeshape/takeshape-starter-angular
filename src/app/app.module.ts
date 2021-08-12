15d1fb187b691645c551c4b41c38801677f216c7
S3Prototype <liuqahs15@gmail.com>
2021-08-12 17:12:30 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from  '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductsComponent } from './products/products.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    ProductsComponent,
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
